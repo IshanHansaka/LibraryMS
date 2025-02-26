71fa9f2fbddea09ade40ce1a0bcfced1da2beb76
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-26 10:52:20 +0530

    feat: test healtCheck of BookController

--- src/main/java/com/DevSprint/LibraryMS/controller/BookController.java
+++ src/main/java/com/DevSprint/LibraryMS/controller/BookController.java
@@ -1,5 +1,15 @@
 package com.DevSprint.LibraryMS.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //handle http req & res
+@RequestMapping("api/v1/books") //industry best practise
 public class BookController {
 
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Book Controller running";
+    }
 }
