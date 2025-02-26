71c2bba190ddb7b569a76b5ed78280aa587216cb
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-26 13:33:46 +0530

    feat: create req to get all book

--- src/main/java/com/DevSprint/LibraryMS/controller/BookController.java
+++ src/main/java/com/DevSprint/LibraryMS/controller/BookController.java
@@ -1,7 +1,11 @@
 package com.DevSprint.LibraryMS.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.DevSprint.LibraryMS.dto.BookDTO;
 
-@RestController //handle http req & res
-@RequestMapping("api/v1/books") //industry best practise
+@RestController // handle http req & res
+@RequestMapping("api/v1/books") // industry best practise
 public class BookController {
 
     @GetMapping("/health")
@@ -47,17 +51,61 @@ public class BookController {
     public ResponseEntity<BookDTO> getBookById(@PathVariable String bookId) {
         System.out.println("Get book by id for " + bookId);
         return ResponseEntity.ok(new BookDTO(
-            "12345",
-            "Effective Java",
-            "Joshua Bloch",
-            "3rd",
-            "Addison-Wesley",
-            "978-0134685991",
-            45.99,
-            100,
-            75,
-            "2025-02-26",
-            "14:30:00"
-        ));
+                "12345",
+                "Effective Java",
+                "Joshua Bloch",
+                "3rd",
+                "Addison-Wesley",
+                "978-0134685991",
+                45.99,
+                100,
+                75,
+                "2025-02-26",
+                "14:30:00"));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> getAllBooks() {
+        List<BookDTO> bookList = new ArrayList<>();
+        bookList.add(new BookDTO(
+                "00123",
+                "Clean Code",
+                "Robert C. Martin",
+                "1st",
+                "Prentice Hall",
+                "978-0132350884",
+                40.00,
+                200,
+                150,
+                "2025-03-01",
+                "10:00:00"));
+
+        bookList.add(new BookDTO(
+                "00456",
+                "Java: The Complete Reference",
+                "Herbert Schildt",
+                "10th",
+                "McGraw-Hill",
+                "978-1260440232",
+                60.99,
+                150,
+                130,
+                "2025-03-02",
+                "11:30:00"));
+
+        bookList.add(new BookDTO(
+                "00789",
+                "Design Patterns: Elements of Reusable Object-Oriented Software",
+                "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
+                "1st",
+                "Addison-Wesley",
+                "978-0201633610",
+                55.50,
+                80,
+                60,
+                "2025-03-03",
+                "12:45:00"));
+        
+        return ResponseEntity.ok(bookList);
     }
 }
