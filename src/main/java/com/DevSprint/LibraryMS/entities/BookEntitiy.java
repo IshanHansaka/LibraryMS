1b0850d647651b76e25debc17ac826f3e9410ab3
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-26 23:58:11 +0530

    feat: create book entity

---
+++ src/main/java/com/DevSprint/LibraryMS/entities/BookEntitiy.java
@@ -0,0 +1,37 @@
+package com.DevSprint.LibraryMS.entities;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "book")
+public class BookEntitiy {
+    @Id
+    private String bookId;
+    private String bookName;
+    private String author;
+    private String edition;
+    private String publisher;
+    private String isbn;
+    private Double price;
+    private Integer totalQty;
+    private Integer availableQty;
+    private LocalDate lastUpdateDate;    
+    private Time lastUpdateTime;
+    
+    @OneToMany(mappedBy = "book",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<LendingEntity> lendings;
+}
