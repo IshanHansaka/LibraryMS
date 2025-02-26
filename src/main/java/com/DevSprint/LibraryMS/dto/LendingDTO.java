51f6f2985797b9ceaa2fb03d51e2e67a84237ebf
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-26 21:26:53 +0530

    chore: change lending & return date data type from String to Local Date

--- src/main/java/com/DevSprint/LibraryMS/dto/LendingDTO.java
+++ src/main/java/com/DevSprint/LibraryMS/dto/LendingDTO.java
@@ -1,6 +1,7 @@
 package com.DevSprint.LibraryMS.dto;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,8 +14,8 @@ public class LendingDTO implements Serializable{
     private String lendingId;
     private String book;
     private String member;
-    private String lendingDate;
-    private String returnDate;
+    private LocalDate lendingDate;
+    private LocalDate returnDate;
     private Boolean isActiveLending;
     private Long overdueDays;
     private Double fineAmount;
