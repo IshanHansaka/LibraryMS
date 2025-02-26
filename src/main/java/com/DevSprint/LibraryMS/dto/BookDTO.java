0f9ac66ee2896f79b12d7f0f8c63031651f2fb42
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-26 11:22:20 +0530

    feat: create Book DTO

---
+++ src/main/java/com/DevSprint/LibraryMS/dto/BookDTO.java
@@ -0,0 +1,26 @@
+package com.DevSprint.LibraryMS.dto;
+
+import java.io.Serializable;
+import java.sql.Time;
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class BookDTO implements Serializable{
+    private String bookId;
+    private String bookName;
+    private String author;
+    private String edition;
+    private String publisher;
+    private String isbn;
+    private Double price;
+    private Integer totalQty;
+    private Integer availableQty;
+    private LocalDate lastUpdateDate;    
+    private Time lastUpdateTime;
+}
