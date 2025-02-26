9e326f7df139638f53ed8f51f5d0c6d74b92064a
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-26 20:01:44 +0530

    feat: add constructor injection with Staffservice interface

--- src/main/java/com/DevSprint/LibraryMS/controller/StaffController.java
+++ src/main/java/com/DevSprint/LibraryMS/controller/StaffController.java
@@ -1,5 +1,7 @@
 package com.DevSprint.LibraryMS.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,13 +14,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.DevSprint.LibraryMS.dto.Role;
 import com.DevSprint.LibraryMS.dto.StaffDTO;
+import com.DevSprint.LibraryMS.service.StaffService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("api/v1/staff")
+@RequiredArgsConstructor // constructor injection
 public class StaffController {
+
+    public final StaffService staffService;
 
     @GetMapping("/health")
     public String healthCheck() {
@@ -27,33 +33,29 @@ public class StaffController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> addStaffMember(@RequestBody StaffDTO staffDTO) {
-        System.out.println(staffDTO);
+        staffService.addStaffMember(staffDTO);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping
     public ResponseEntity<Void> deleteStaffMember(@RequestParam("staffId") String staffId) {
-        System.out.println(staffId);
+        staffService.deleteStaffMember(staffId);
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping(value = "/{staffId}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> updateStaffMember(@PathVariable String staffId, @RequestBody StaffDTO staffDTO) {
-        System.out.println(staffId);
-        System.out.println(staffDTO);
+        staffService.updateStaffMember(staffId, staffDTO);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{staffId}")
     public ResponseEntity<StaffDTO> getStaffById(@PathVariable String staffId) {
-        System.out.println("Get staff by id for " + staffId);
-        return ResponseEntity.ok(new StaffDTO(
-            "S12345",
-            "John",
-            "Doe",
-            "john.doe@example.com",
-            "2025-02-26",
-            "+1234567890",
-            Role.ADMIN));
+        return ResponseEntity.ok(staffService.getStaffById(staffId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StaffDTO>> getAllStaff() {
+        return ResponseEntity.ok(staffService.getAllStaff());
     }
 }
