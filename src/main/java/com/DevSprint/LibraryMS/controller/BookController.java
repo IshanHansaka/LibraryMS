fd934e77b47592637a0061fc66c576060a83e38c
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-26 13:41:53 +0530

    feat: create rest controllers & request mappings

--- src/main/java/com/DevSprint/LibraryMS/controller/BookController.java
+++ src/main/java/com/DevSprint/LibraryMS/controller/BookController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/DevSprint/LibraryMS/controller/LendingController.java
+++ src/main/java/com/DevSprint/LibraryMS/controller/LendingController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/staff")
-public class StaffController {
+@RequestMapping("api/v1/lendings")
+public class LendingController {
 
 }
