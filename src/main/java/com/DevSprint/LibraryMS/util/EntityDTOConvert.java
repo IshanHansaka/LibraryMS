2c977d09b708557e491f82975531e9ec81f35e0e
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-27 18:15:04 +0530

    fix: add component annotation and change method names

--- src/main/java/com/DevSprint/LibraryMS/util/EntityDTOConvert.java
+++ src/main/java/com/DevSprint/LibraryMS/util/EntityDTOConvert.java
@@ -4,27 +4,30 @@ import java.util.List;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
+import org.springframework.stereotype.Component;
 
 import com.DevSprint.LibraryMS.dto.BookDTO;
 import com.DevSprint.LibraryMS.entities.BookEntitiy;
 
 import lombok.RequiredArgsConstructor;
 
+@Component
 @RequiredArgsConstructor // Constructor injection
 public class EntityDTOConvert {
 
     private final ModelMapper modelMapper;
 
-    //Book
-    public BookEntitiy convertBookDTOToBookEntity(BookDTO bookDTO) {
+    // Book
+    public BookEntitiy ToBookEntity(BookDTO bookDTO) {
         return modelMapper.map(bookDTO, BookEntitiy.class);
     }
 
-    public BookDTO convertBookEntityToBookDTO(BookEntitiy bookEntitiy) {
+    public BookDTO ToBookDTO(BookEntitiy bookEntitiy) {
         return modelMapper.map(bookEntitiy, BookDTO.class);
     }
 
-    public List<BookDTO> convertBookEntityListToBookDTOList(List<BookEntitiy> bookEntitiyList) {
-        return modelMapper.map(bookEntitiyList, new TypeToken<List<BookDTO>>(){}.getType());
+    public List<BookDTO> ToBookDTOList(List<BookEntitiy> bookEntitiyList) {
+        return modelMapper.map(bookEntitiyList, new TypeToken<List<BookDTO>>() {
+        }.getType());
     }
 }
