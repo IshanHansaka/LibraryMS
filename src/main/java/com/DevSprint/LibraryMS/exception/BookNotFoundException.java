3ad4a3549d3ae6437c6fca55b0c4821685173838
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-27 20:11:41 +0530

    feat: create exception package to add custom exceptions

---
+++ src/main/java/com/DevSprint/LibraryMS/exception/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.DevSprint.LibraryMS.exception;
+
+public class BookNotFoundException extends RuntimeException{
+
+    public BookNotFoundException(String string) {
+        //TODO Auto-generated constructor stub
+    }
+
+}
