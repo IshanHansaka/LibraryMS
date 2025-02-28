b785fdb45fe47c5366a2684bfa778bed6340854e
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-28 12:16:48 +0530

    feat: create member not found custome exception

---
+++ src/main/java/com/DevSprint/LibraryMS/exception/MemberNotFoundException.java
@@ -0,0 +1,8 @@
+package com.DevSprint.LibraryMS.exception;
+
+public class MemberNotFoundException extends RuntimeException{
+
+    public MemberNotFoundException(String string) {
+        //TODO Auto-generated constructor stub
+    }
+}
