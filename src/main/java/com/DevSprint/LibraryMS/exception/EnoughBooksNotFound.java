01fb5958fb4f8b290619d70201604d2f4d8ce634
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-28 14:55:33 +0530

    feat: create custom exception to return book count are not enough to proceed lending

---
+++ src/main/java/com/DevSprint/LibraryMS/exception/EnoughBooksNotFound.java
@@ -0,0 +1,9 @@
+package com.DevSprint.LibraryMS.exception;
+
+public class EnoughBooksNotFound extends RuntimeException{
+
+    public EnoughBooksNotFound(String string) {
+        //TODO Auto-generated constructor stub
+    }
+
+}
