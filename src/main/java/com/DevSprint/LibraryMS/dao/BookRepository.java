70265029ada35bf41560a4e3a0ddebf7794368e6
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-28 16:27:32 +0530

    feat: add JP Query to decreament book quantity by one when lending book

--- src/main/java/com/DevSprint/LibraryMS/dao/BookRepository.java
+++ src/main/java/com/DevSprint/LibraryMS/dao/BookRepository.java
@@ -8,9 +8,13 @@ import org.springframework.stereotype.Repository;
 import com.DevSprint.LibraryMS.entities.BookEntitiy;
 
 @Repository
-public interface BookRepository extends JpaRepository<BookEntitiy, String>{
+public interface BookRepository extends JpaRepository<BookEntitiy, String> {
 
     // JPQL Query
     @Query("SELECT SUM(b.availableQty) FROM BookEntity b WHERE b.bookID = :bookID")
     int availableQty(@Param("bookID") String bookId);
+
+    // Deduct book count based on lending
+    @Query("UPDATE BookEntity b SET b.availabeQty = b.availabeQty - 1 WHERE b.bookId = :bookId AND b.availabeQty > 0")
+    int deductBasedOnLending(@Param("bookId") String bookId);
 }
