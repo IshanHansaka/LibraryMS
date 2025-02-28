9a62c623f7c3ed750558a05d49b9a650511d1702
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-28 17:01:50 +0530

    chore: remove EnoughBooksNotFound custom exception

--- src/main/java/com/DevSprint/LibraryMS/exception/EnoughBooksNotFound.java
+++
@@ -1,9 +0,0 @@
-package com.DevSprint.LibraryMS.exception;
-
-public class EnoughBooksNotFound extends RuntimeException{
-
-    public EnoughBooksNotFound(String string) {
-        //TODO Auto-generated constructor stub
-    }
-
-}
