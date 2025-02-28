97e0a860e762d342adf845de987cf8d058b63c6f
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-28 23:32:33 +0530

    chore: change class name of custom exception to lending data not found exception

---
+++ src/main/java/com/DevSprint/LibraryMS/exception/LendingDataNotFoundException.java
@@ -0,0 +1,8 @@
+package com.DevSprint.LibraryMS.exception;
+
+public class LendingDataNotFoundException extends RuntimeException{
+
+    public LendingDataNotFoundException(String string) {
+        //TODO Auto-generated constructor stub
+    }
+}
