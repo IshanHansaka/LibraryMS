41015096f22f4ae90e5b49a9a1f0a9d5a9a5fa2a
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-28 16:26:24 +0530

    feat: create custom exception

---
+++ src/main/java/com/DevSprint/LibraryMS/exception/DataPersistException.java
@@ -0,0 +1,8 @@
+package com.DevSprint.LibraryMS.exception;
+
+public class DataPersistException extends RuntimeException{
+
+    public DataPersistException(String string) {
+        //TODO Auto-generated constructor stub
+    }
+}
