8dbbe4773eceea2c266e81da92531e74abf6e6c2
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-28 14:00:37 +0530

    feat: create staff not found custome exception

---
+++ src/main/java/com/DevSprint/LibraryMS/exception/StaffNotFoundException.java
@@ -0,0 +1,8 @@
+package com.DevSprint.LibraryMS.exception;
+
+public class StaffNotFoundException extends RuntimeException{
+
+    public StaffNotFoundException(String string) {
+        //TODO Auto-generated constructor stub
+    }
+}
