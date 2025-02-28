7d4d3bae933292dbd4f103a711baf5139272ba16
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-28 18:53:10 +0530

    fix: fixed issues

--- src/main/java/com/DevSprint/LibraryMS/dao/BookRepository.java
+++ src/main/java/com/DevSprint/LibraryMS/dao/BookRepository.java
@@ -1,20 +1,24 @@
 package com.DevSprint.LibraryMS.dao;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.DevSprint.LibraryMS.entities.BookEntitiy;
+import com.DevSprint.LibraryMS.entities.BookEntity;
 
 @Repository
-public interface BookRepository extends JpaRepository<BookEntitiy, String> {
+public interface BookRepository extends JpaRepository<BookEntity, String> {
 
     // JPQL Query
-    @Query("SELECT SUM(b.availableQty) FROM BookEntity b WHERE b.bookID = :bookID")
-    int availableQty(@Param("bookID") String bookId);
+    @Query("SELECT b.availableQty FROM BookEntity b WHERE b.bookID = :bookID")
+    Integer availableQty(@Param("bookID") String bookId);
 
     // Deduct book count based on lending
-    @Query("UPDATE BookEntity b SET b.availabeQty = b.availabeQty - 1 WHERE b.bookId = :bookId AND b.availabeQty > 0")
-    int deductBasedOnLending(@Param("bookId") String bookId);
+    @Modifying
+    @Transactional
+    @Query("UPDATE BookEntity b SET b.availableQty = b.availableQty - 1 WHERE b.bookId = :bookId AND b.availableQty > 0")
+    Integer deductBasedOnLending(@Param("bookId") String bookId);
 }
