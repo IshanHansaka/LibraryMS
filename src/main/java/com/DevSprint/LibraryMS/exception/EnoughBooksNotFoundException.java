f49eb470bd32192dcad2b12414d8070f3d5454fb
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-28 17:01:03 +0530

    feat: create new custom exception

---
+++ src/main/java/com/DevSprint/LibraryMS/exception/EnoughBooksNotFoundException.java
@@ -0,0 +1,9 @@
+package com.DevSprint.LibraryMS.exception;
+
+public class EnoughBooksNotFoundException extends RuntimeException{
+
+    public EnoughBooksNotFoundException(String string) {
+        //TODO Auto-generated constructor stub
+    }
+
+}
