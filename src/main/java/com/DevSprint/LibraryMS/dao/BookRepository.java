58bd0881bdb4e6bc3b219b69c9e027283a4caee6
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-02-28 14:52:42 +0530

    feat: creat custom JP Query to get sum of available book quantity

--- src/main/java/com/DevSprint/LibraryMS/dao/BookRepository.java
+++ src/main/java/com/DevSprint/LibraryMS/dao/BookRepository.java
@@ -1,10 +1,16 @@
 package com.DevSprint.LibraryMS.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.DevSprint.LibraryMS.entities.BookEntitiy;
 
 @Repository
 public interface BookRepository extends JpaRepository<BookEntitiy, String>{
+
+    // JPQL Query
+    @Query("SELECT SUM(b.availableQty) FROM BookEntity b WHERE b.bookID = :bookID")
+    int availableQty(@Param("bookID") String bookId);
 }
